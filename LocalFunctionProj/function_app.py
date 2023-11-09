e6f06a179c8bd351b8bc7c0b66327c1302895b1c
EugeneHsiung <Eugene.hsiung@stonybrook.edu>
2023-11-09 02:32:53 +0000

    update python package

--- LocalFunctionProj/function_app.py
+++ LocalFunctionProj/function_app.py
@@ -1,6 +1,23 @@
 import azure.functions as func
-import datetime
 import json
 import logging
 
-app = func.FunctionApp()
+app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
+
+@app.route(route="home")
+def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
+
+    name = req.params.get('name', 'World')
+    lastname = req.args.get('lastname', 'no last name provided')
+    nameCapital = name.upper()
+    lastnameCapital = lastname.upper()
+
+    response = {
+        "message": f'Hello {nameCapital} {lastnameCapital}!'
+    }
+
+    return func.HttpResponse(
+        json.dumps(response),
+        status_code=200
+        )
+# type in: /home?name=John&lastname=Smith (example)
